88cb44d3a61981394012c031ddcedef569a14e88
Nick Patrick <n.patrick@outlook.com>
2019-03-26 15:13:34 +0000

    Update theme.js

--- app/config/theme.js
+++ app/config/theme.js
@@ -16,7 +16,7 @@ module.exports = {
     headerColor: '#fff'
   },
   firebaseConfig: {
-    apiKey: "AIzaSyAA4BsLduX1c0wnXN_N7RWXrIQD24VoMVA",
+    apiKey: "AIzaSyAA4BsLduX1c0wnXN_N7RWXrIQD24VoMVA", // now deleted
     authDomain: "netflixtitles.firebaseapp.com",
     databaseURL: "https://netflixtitles.firebaseio.com",
     storageBucket: "netflixtitles.appspot.com",
